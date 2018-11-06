2bf686684aa40cb06ec75ca91a33b11532bfae5a
Ly111999 <39553327+Ly111999@users.noreply.github.com>
2018-11-06 13:42:20 +0700

    productApps

---
+++ ASP.Net/01_ASP/01_ASP/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using _01_ASP.Models;
+
+namespace _01_ASP.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
